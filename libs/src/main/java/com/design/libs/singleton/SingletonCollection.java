49ca8cbea59cd3ecb77b3adffe290f86f01d1f6c
sutongsheng <mr_su3200@sina.com>
2020-04-25 22:27:15 +0800

    1、mac提交修改测试

--- libs/src/main/java/com/design/libs/singleton/SingletonCollection.java
+++ libs/src/main/java/com/design/libs/singleton/SingletonCollection.java
@@ -11,6 +11,7 @@ import java.util.Map;
 public class SingletonCollection {
     private static Map<String, Object> singletonmaps = new HashMap<>();
 
+    //
     private SingletonCollection() {
     }
 
