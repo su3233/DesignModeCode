18ecf3ae1c54d6bdacfcb2b3470afa6500fab060
sutongsheng <mr_su3200@sina.com>
2020-04-26 09:30:25 +0800

    205提交测试

--- libs/src/main/java/com/design/libs/MyClass.java
+++ libs/src/main/java/com/design/libs/MyClass.java
@@ -1,4 +1,7 @@
 package com.design.libs;
 
 public class MyClass {
+    public static void main(String[] args) {
+
+    }
 }
--- libs/src/main/java/com/design/libs/singleton/StaticSingleton.java
+++ libs/src/main/java/com/design/libs/singleton/StaticSingleton.java
@@ -3,7 +3,7 @@ package com.design.libs.singleton;
 /**
  * @auther:sutongsheng
  * @date:2020/4/25 14:08
- * @description:静态内部类的形式
+ * @description:    静态内部类的形式
  */
 public class StaticSingleton {
     private StaticSingleton() {
